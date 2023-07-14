190076a130fbe6ce28761fe15bfb523fc35388ee
adriano ansaloni <adrianoansaloni@gmail.com>
2023-07-14 14:00:36 -0300

    Definição das rotas para tarefas

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::put('/listas/{lista}', [ListaController::class, 'update'])->name('listas.
 
 // Rota para excluir uma lista
 Route::delete('/listas/{lista}', [ListaController::class, 'destroy'])->name('listas.destroy');
+
+Route::post('/tarefas', [TarefaController::class, 'store'])->name('tarefas.store');
+Route::put('/tarefas/{tarefa}', [TarefaController::class, 'update'])->name('tarefas.update');
+Route::delete('/tarefas/{tarefa}', [TarefaController::class, 'destroy'])->name('tarefas.destroy');
