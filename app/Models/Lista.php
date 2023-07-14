1cba841a33a3b7e9b0a74ad79a15c7a14adf3049
adriano ansaloni <adrianoansaloni@gmail.com>
2023-07-14 13:56:17 -0300

    Funções model Lista

--- app/Models/Lista.php
+++ app/Models/Lista.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Lista extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'completed'];
+
+    public function tarefas()
+    {
+        return $this->hasMany(Tarefa::class);
+    }
+
+    public function getCompletedTasksAttribute()
+    {
+        return $this->tarefas()->where('completed', true)->count();
+    }
+
+    public function getTotalTasksAttribute()
+    {
+        return $this->tarefas()->count();
+    }
 }
